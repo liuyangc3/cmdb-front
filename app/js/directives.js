1d53354da41c8ae16ea24dfd0c8e0641e9860f8f
liuyang <liuyangc3@126.com>
2015-10-20 14:02:52 +0800

    优化 service 展示页面

--- app/js/directives.js
+++ app/js/directives.js
@@ -10,20 +10,21 @@ angular.module('cmdb')
             templateUrl: 'views/service/directive/rowKey.html',
             scope: {
                 bindValue: '=',
+                pushAlert: '&',
                 deleteRow: '&',
                 modifyTableKey: '&'
             },
             replace: true,
-            link: function (scope) {
+            link: function (scope, element) {
                 // 并行指令通知机制
                 //scope.$parent.$broadcast('keyModify', someValue);
+
                 scope.reservedKey = function () {
                     // 不能删除,不能编辑的 key
                     return (scope.bindValue == 'name' || scope.bindValue == '_rev');
                 };
                 scope.showThis = true;
-                scope.valueBefore = scope.bindValue;
-
+                scope.inputText = scope.bindValue;
                 scope.transEditState = function () {
                     if (!scope.reservedKey()) {
                         scope.showThis = !scope.showThis;
@@ -31,18 +32,18 @@ angular.module('cmdb')
                 };
                 scope.saveKey = function () {
                     scope.showThis = !scope.showThis;
-                    if (scope.bindValue == scope.valueBefore) {
+                    if (scope.inputText == scope.bindValue) {
                         // 未作任何修改
                         return
                     }
                     try {
                         scope.modifyTableKey({
-                            key: scope.valueBefore,
-                            newkey: scope.bindValue
+                            key: scope.bindValue,
+                            newkey: scope.inputText
                         })();
                     } catch (error) {
-                        alert(error); // TODO：提示更友好一些
-                        scope.bindValue = scope.valueBefore;
+                        scope.pushAlert({msg: error})();
+                        scope.inputText = scope.bindValue;
                     }
                 };
             } // link end
@@ -59,9 +60,38 @@ angular.module('cmdb')
             replace: true,
             link: function(scope, element, attr){
                 scope.showThis = true;
+                scope.valueBefore = scope.bindValue;
                 scope.transEditState = function() {
                     if('_rev' != scope.bindKey) {
                         scope.showThis = !scope.showThis;
+                        scope.bindValue = scope.valueBefore;
+                    }
+                };
+            }
+        }}])
+    .directive('projectTableKey',[function(){
+        return {
+            restrict: 'AE',
+            templateUrl: 'views/project/directive/rowKey.html',
+            scope: {
+                bindValue: '='
+            },
+            replace: true,
+            link: function(scope){
+                scope.showThis = true;
+                scope.inputText = scope.bindValue;
+                scope.reservedKey = function() {
+                    // 不能删除的 key
+                    return (scope.bindValue == 'services' || scope.bindValue == '_rev');
+                };
+                scope.isEditable = function() {
+                    // 不能编辑的key
+                    return  scope.bindValue == '_rev';
+                };
+                scope.transEditState = function() {
+                    if('_rev' != scope.bindKey) {
+                        scope.showThis = !scope.showThis;
+                        scope.inputText = scope.bindValue;
                     }
                 };
             }
